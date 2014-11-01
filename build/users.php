a44df2235f0192f3103ac90eacdd35a63094fce9
James Harris <wopiancitizen123@gmail.com>
2014-11-01 01:37:32 +0000

    Solid cover

--- build/users.php
+++ build/users.php
@@ -82,8 +82,9 @@
   </div>
 
   <div id="container">
-    <div id="cover" style="background-image: url('https://s3.amazonaws.com/f.cl.ly/items/062K3X2O2724291l0X0y/cover-default.png')">
-      <div id="avatar" class="spinner">
+    <!--<div id="cover loading" style="background-image: url('https://s3.amazonaws.com/f.cl.ly/items/062K3X2O2724291l0X0y/cover-default.png')">-->
+    <div id="cover loading">
+      <div class="spinner">
         <div class="double-bounce1"></div>
         <div class="double-bounce2"></div>
       </div>
