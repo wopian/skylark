40e6b7576efb13a8e88d4268352cb5ddb621ff48
James Harris <wopiancitizen123@gmail.com>
2014-11-05 15:52:12 +0000

    Make sure it's seconds, not minutes.

--- build/dist/templates/users.php
+++ build/dist/templates/users.php
@@ -70,7 +70,7 @@
     <div class="col-lg-6 col-md-6 col-sm-6 text-right">
       <p class="h1">Watched</p>
       <!--<p class="lead"><?=secondsToTime($data['life_spent_on_anime'])?> of anime</p>-->
-      <p class="lead"><?=seconds2human($data['life_spent_on_anime'])?></p>"
+      <p class="lead"><?=seconds2human($data['life_spent_on_anime']*60)?></p>"
     </div>
 
     <div class="col-lg-12 col-md-12 col-sm-12">
