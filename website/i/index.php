75eaa38dcf1d50a362a4da61bd3fb979fb0b4fae
James Harris <wopiancitizen123@gmail.com>
2014-11-06 21:04:13 +0000

    Fix url :dart:

--- website/i/index.php
+++ website/i/index.php
@@ -2,7 +2,7 @@
 header("Content-Type: text/text");
 
 $key = "befd7918ae80d68cfe03530e453ceb587a053144";
-$uploadhost = "http://wopian.me/i";
+$uploadhost = "http://wopian.me/i/";
 $redirect = "http://wopian.me/i/denied";
 
 #Comment this next line if you want Robots to index this site.
