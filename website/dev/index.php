32b73765948d56fad5252ae94ecc931512a5f2a2
James Harris <wopiancitizen123@gmail.com>
2014-11-07 23:05:38 +0000

    Test

--- website/dev/index.php
+++ website/dev/index.php
@@ -4,6 +4,7 @@
 <head>
     <title>Hummingbird Tools</title>
     <link rel="stylesheet" href="/assets/css/flexboxgrid.css" type="text/css" />
+    <link rel="stylesheet" href="/assets/css/waves.min.css" type="text/css" />
     <link rel="stylesheet" href="/assets/css/main.css" type="text/css" />
 </head>
 
@@ -17,8 +18,10 @@
 
         <div class="row">
             <div class="col-xs-12 col-sm-4 col-md-3">
-                <div class="panel">
-                    <img src="https://static.hummingbird.me/anime/poster_images/000/008/147/large/b0012149_53d85a49c60c2.jpg?1408463649">
+                <div class="panel waves-effect waves-block">
+                    <a href="http://hummingbird.me/anime/kiseijuu">
+                        <img src="https://static.hummingbird.me/anime/poster_images/000/008/147/large/b0012149_53d85a49c60c2.jpg?1408463649">
+                    </a>
                     <div class="caption">
                         <p class="lead">Kiseijuu: Sei no Kakuritsu</p>
                         <p>Watched 5 of 24 episodes</p>
@@ -362,6 +365,11 @@
     </main>
 
     <img id="dploy" src="https://wopian.dploy.io/badge/13023223950720/13284.png" alt="Deployment status from dploy.io">
+
+    <script type="text/javascript" src="/assets/js/waves.min.js"></script>
+    <script type="text/javascript">
+        Waves.displayEffect();
+    </script>
 
 </body>
 
