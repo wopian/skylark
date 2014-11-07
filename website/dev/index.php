d9f6b42f7d271fcc2e60bad85ff07df43078e2bd
James Harris <wopiancitizen123@gmail.com>
2014-11-07 22:30:21 +0000

    Open Sans

--- website/dev/index.php
+++ website/dev/index.php
@@ -19,9 +19,11 @@
             <div class="col-md-3">
                 <div class="panel">
                     <img src="https://static.hummingbird.me/anime/poster_images/000/008/147/large/b0012149_53d85a49c60c2.jpg?1408463649">
-                    <p class="lead">Kiseijuu: Sei no Kakuritsu</p>
-                    <p>Watched 5 of 24 episodes</p>
-                    <p>18 hours ago</p>
+                    <div class="caption">
+                        <p class="lead">Kiseijuu: Sei no Kakuritsu</p>
+                        <p>Watched 5 of 24 episodes</p>
+                        <p>18 hours ago</p>
+                    </div>
                 </div>
             </div>
         </div>
