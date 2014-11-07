a63d503c4c009271090268879ec85fe14ba297a3
James Harris <wopiancitizen123@gmail.com>
2014-11-07 22:24:08 +0000

    Img

--- website/dev/index.php
+++ website/dev/index.php
@@ -18,7 +18,7 @@
         <div class="row">
             <div class="col-md-3">
                 <div class="panel">
-                    <img src="http://hummingbird.me/anime/kiseijuu">
+                    <img src="https://static.hummingbird.me/anime/poster_images/000/008/147/large/b0012149_53d85a49c60c2.jpg?1408463649">
                     <p class="lead">Kiseijuu: Sei no Kakuritsu</p>
                     <p>Watched 5 of 24 episodes</p>
                     <p>18 hours ago</p>
