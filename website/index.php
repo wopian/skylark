430b5515adc2364c6020fe43446c4b125601f329
James Harris <wopiancitizen123@gmail.com>
2014-11-07 17:36:10 +0000

    wopian.me

--- website/index.php
+++ website/index.php
@@ -1,1 +1,12 @@
-<?php header('Location: http://wopian.me/hb');
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Wopian</title>
+</head>
+
+<body>
+    Nothing here yet, Hummingbird Tools is located <a href="http://hb.wopian.me">here</a>.
+</body>
+
+</html>
