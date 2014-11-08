70a7be02c867e3ee926d3044e721b799f49cc81a
James Harris <wopiancitizen123@gmail.com>
2014-11-08 17:21:18 +0000

    Oops

--- website/dev/assets/modules/index.php
+++ website/dev/assets/modules/index.php
@@ -1,30 +1,3 @@
-<!DOCTYPE html>
-<html>
-
-<head>
-    <title>Hummingbird Tools</title>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="Hummingbird Tools hosts a variety of tools and stats for Hummingbird.">
-    <meta name="keywords" content="Hummingbird,Tool,Tools,Anime,Manga,API,User Stats,Library,User Library,Cover,Cover Images">
-    <meta name="author" content="James Harris">
-
-    <meta property="og:image" content="http://9.dev.boomcraft.co.uk/dist/images/avatar_teal.png" />
-    <meta property="og:url" content="http://9.dev.boomcraft.co.uk" />
-    <meta property="og:title" content="Hummingbird Tools" />
-
-    <meta name="twitter:card" content="summary" />
-    <meta name="twitter:site" content="@hb_tools" />
-    <meta name="twitter:title" content="Hummingbird Tools" />
-    <meta name="twitter:description" content="A variety of tools and stats for Hummingbird." />
-    <meta name="twitter:image" content="http://9.dev.boomcraft.co.uk/dist/images/twitter_teal.png" />
-    <meta name="twitter:url" content="http://9.dev.boomcraft.co.uk" />
-
-    <link href="/assets/css/main.css" rel="stylesheet">
-</head>
-
-<body>
-
     <div class="navbar navbar-material-teal">
         <div class="container">
             <div class="navbar-header">
@@ -161,27 +134,3 @@
             </div>
         </div>
     </div>
-
-    <svg class="spinner" width="66px" height="66px" viewBox="0 0 66 66" xmlns="http://www.w3.org/2000/svg">
-        <circle class="path" fill="none" stroke-width="6" stroke-linecap="round" cx="33" cy="33" r="30"></circle>
-    </svg>
-
-    <img id="dploy" src="https://wopian.dploy.io/badge/13023223950720/13284.png" alt="Deployment status from dploy.io">
-
-    <script src="//code.jquery.com/jquery-1.10.2.min.js"></script>
-    <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
-
-    <script src="/assets/javascript/ripples.min.js"></script>
-    <script src="/assets/javascript/material.min.js"></script>
-
-    <script>
-        $(document).ready(function() {
-            $.material.init();
-        });
-    </script>
-
-    <script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-545296d61bde8abb" async="async"></script>
-
-</body>
-
-</html>
