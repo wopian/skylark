da380dbf2406053f25c13e71712d1cf5841bd40e
James Harris <wopiancitizen123@gmail.com>
2014-11-17 22:25:36 +0000

    oops

--- website/hb/users.php
+++ website/hb/users.php
@@ -15,7 +15,7 @@
     function format_name($x = 'full') {
         $user_name = $user_data['name'];
 
-        switch {
+        switch ($x) {
             case 'full':
                 return ucfirst($user_name);
                 break;
@@ -32,22 +32,22 @@
 <html>
 
 <head>
-  <title><?=format_name('plural')?> Profile - Hummingbird Tools</title>
+  <title><?format_name('plural')?> Profile - Hummingbird Tools</title>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
-  <meta name="description" content="View <?=format_name('plural')?> profile.">
-  <meta name="keywords" content="Hummingbird,Tool,Tools,Anime,Manga,API,Profile,User,Stats,<?=format_name()?>">
+  <meta name="description" content="View <?format_name('plural')?> profile.">
+  <meta name="keywords" content="Hummingbird,Tool,Tools,Anime,Manga,API,Profile,User,Stats,<?format_name()?>">
   <meta name="author" content="James Harris">
 
   <meta property="og:image" content="<?=$user_data['avatar']?>" />
   <meta property="og:url" content="//9.dev.boomcraft.co.uk/<?=$user?>" />
-  <meta property="og:title" content="<?=format_name('plural')?> Profile - Hummingbird Tools" />
+  <meta property="og:title" content="<?format_name('plural')?> Profile - Hummingbird Tools" />
 
   <meta name="twitter:card" content="summary" />
   <meta name="twitter:site" content="@hb_tools" />
-  <meta name="twitter:title" content="<?=format_name('plural')?> Profile - Hummingbird Tools" />
-  <meta name="twitter:description" content="View <?=format_name('plural')?> profile." />
+  <meta name="twitter:title" content="<?format_name('plural')?> Profile - Hummingbird Tools" />
+  <meta name="twitter:description" content="View <?format_name('plural')?> profile." />
   <meta name="twitter:image" content="<?=$user_data['avatar']?>" />
   <meta name="twitter:url" content="//9.dev.boomcraft.co.uk/<?=$user?>" />
 
