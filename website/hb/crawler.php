454a8e82835f18962025bbd1c49a7ff4957de7d9
James Harris <wopiancitizen123@gmail.com>
2014-11-25 23:37:41 +0000

    start at 0

--- website/hb/crawler.php
+++ website/hb/crawler.php
@@ -10,13 +10,13 @@
         die('There was an error running the query [' . $db->error . ']');
     }
 
-    $a = mysqli_fetch_row($result)[1];
+    $a = mysqli_fetch_row($result)[0];
 
     $sql = "SELECT COUNT(*) FROM `users` WHERE `crawled` = 0";
     if(!$result = $db->query($sql)){
         die('There was an error running the query [' . $db->error . ']');
     }
 
-    $b = mysqli_fetch_row($result)[1];
+    $b = mysqli_fetch_row($result)[0];
 
     echo "$b of $a users processed.";
