cf5823a88fc330ed3f679f3d5def341b79498879
James Harris <wopiancitizen123@gmail.com>
2014-11-26 21:30:21 +0000

    test

--- website/hb/assets/modules/info/crawl_users.php
+++ website/hb/assets/modules/info/crawl_users.php
@@ -1,18 +1,56 @@
 <?php
 
+    # Database connection
     $dbhost     = "localhost";
     $dbname     = "bobstudi_hummingbird";
     $dbuser     = "bobstudi_humming";
     $dbpass     = "music195";
-
-    # database connection
     $conn = new PDO("mysql:host=$dbhost;dbname=$dbname",$dbuser,$dbpass,array(PDO::ATTR_PERSISTENT => true));
 
-    $sql = "SELECT `name`, `crawled` FROM `users` ORDER BY `crawled` ASC LIMIT 5";
+    # Setup default values
+    $users = array();
+
+    $sql = "SELECT `name` FROM `users` ORDER BY `crawled` ASC LIMIT 5";
     $q = $conn->query($sql);
-    while($r = $q->fetch()){
-        echo $r['name'] . "<br>";
+    while($r = $q->fetch()) {
+        $users[] = $r['name']
         #print_r($r);
+    }
+    print_r($users);
+
+    foreach($users as $usr) {
+        $name = $usr['name'];
+
+        $sql = "UPDATE `users`
+                SET `crawled` + 1
+                WHERE `name` = :name";
+        $q = $conn->prepare($sql);
+        $q->execute(array(':name'=>$name));
+
+        $total = 0;
+        for ($x=1, $x<=1000; $x++) {
+            $url = "https://hummingbird.me/users?followers_of=$user&pages=$x";
+            $json = file_get_contents($url);
+            $data = json_decode($json, true);
+
+            if (!empty($data['users'][0])) {
+                $count = count($data['users'])-1;
+                $total .= $count;
+
+                for ($y=0; $y<=$count; $y++) {
+                    $names = strtolower($data['users'][$y]['id']);
+                    $sql = "INSERT INTO `users` (`name`)
+                            VALUES (:name)
+                            ON DUPLICATE KEY UPDATE `name` = :name";
+                    $q = $conn->prepare($sql);
+                    $q->execute(array(':name'=>$names));
+                }
+            } else {
+                break;
+            }
+        }
+
+        echo "Added $total users from $name<br>";
     }
 
 /*
