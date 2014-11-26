698530a9908a4751e511e57546235f534f864117
James Harris <wopiancitizen123@gmail.com>
2014-11-26 19:11:45 +0000

    cookie

--- website/kuso.php
+++ website/kuso.php
@@ -2,6 +2,10 @@
 
     $uid = 1;
 
-    setcookie('uid',$uid,time()+(86400*365));
+    setcookie('uid', $uid, time() + (86400 * 365));
 
-    echo "Cookie OK: ".$_COOKIE['uid'].".";
+    $cookie = $_COOKIE['uid'];
+
+    echo "Cookie OK: $cookie.";
+    echo "<br>";
+    echo "Cookie OK: " . $_COOKIE['uid'] . ".";
