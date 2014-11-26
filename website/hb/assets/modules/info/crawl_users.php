cf671c5a0897d5921d9479fe8fb6c842eca82f97
James Harris <wopiancitizen123@gmail.com>
2014-11-26 21:41:03 +0000

    fix missing colon

--- website/hb/assets/modules/info/crawl_users.php
+++ website/hb/assets/modules/info/crawl_users.php
@@ -13,7 +13,7 @@
     $sql = "SELECT `name` FROM `users` ORDER BY `crawled` ASC LIMIT 5";
     $q = $conn->query($sql);
     while($r = $q->fetch()) {
-        $users[] = $r['name']
+        $users[] = $r['name'];
         #print_r($r);
     }
     print_r($users);
@@ -52,54 +52,3 @@
 
         echo "Added $total users from $name<br>";
     }
-
-/*
-    try {
-
-        #foreach($dbh->query('SELECT `id`, `name`, `crawled` FROM `users` ORDER BY `crawled` ASC LIMIT 1') as $row) {
-        #    $user = $row[0];
-        #}
-        foreach($dbh->query('UPDATE `users` SET `crawled` = 1 WHERE `name` = $user') as $row) {
-        }
-        foreach($dbh->query('SELECT COUNT(*) FROM `users` WHERE `crawled` = 0') as $row) {
-            $uncrawled = $row[0];
-        }
-        #echo '<pre>';
-        #foreach($dbh->query('SELECT * from `users`') as $row) {
-        #    print_r($row);
-        #}
-        $dbh = null;
-        echo '</pre>';
-    } catch (PDOException $e) {
-        print "Error!: " . $e->getMessage() . "<br/>";
-        die();
-    }
-
-    $total = 0;
-    for ($x=1; $x<=1000; $x++) {
-        $url = "https://hummingbird.me/users?followers_of=$user&page=$x";
-        $json = file_get_contents($url);
-        $data = json_decode($json, true);
-
-        if (!empty($data['users'][0])) {
-            $count = count($data['users'])-1;
-            $total = $total + $count;
-
-            for ($y=0; $y<=$count; $y++) {
-                $name = strtolower($data['users'][$y]['id']);
-                try {
-                $dbh->query('INSERT INTO `users` (`name`) VALUES (`$name`) ON DUPLICATE KEY UPDATE `name` = `$name`') as $row) {
-                } catch (PDOException $e) {
-                    print "Error!: " . $e->getMessage() . "<br/>";
-                    die();
-                }
-                $sql = "INSERT INTO `users` (`name`) VALUES ('".$name."') ON DUPLICATE KEY UPDATE `name` = '".$name."'";
-                if(!$result = $db->query($sql)){
-                    die('There was an error running the query [' . $db->error . ']');
-                }
-            }
-        } else {
-            break;
-        }
-    }
-    echo "Added $total users from $user's followers.";
