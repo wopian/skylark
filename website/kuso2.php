cf7131d89fb802274ebd1d8c26364b2ceb4fdffc
James Harris <wopiancitizen123@gmail.com>
2014-11-26 19:21:11 +0000

    cookie

--- website/kuso2.php
+++ website/kuso2.php
@@ -1,16 +1,12 @@
 <?
 
     if (!isset($_COOKIE['uid'])) {
-        $uid = (isset($_POST["userid"])) ? $_POST["userid"] : 0;
+        $uid = (isset($_POST["userid"])) ? $_POST["userid"] : "post";
     } else {
-        $uid = (isset($_COOKIE['uid'])) ? $_COOKIE['uid'] : 0;
+        $uid = (isset($_COOKIE['uid'])) ? $_COOKIE['uid'] : "cookie";
     }
 
     echo "Initial: $uid <br>";
-
-    $uid = 1;
-
-    echo "Set: $uid <br>";
 
     setcookie("uid",$uid,time() + (86400*365));
     echo "Cookie: ".$_COOKIE['uid'].".";
