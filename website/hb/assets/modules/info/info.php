5e79ccb623a0a6f4e0765c87f70dfd2834aad4f0
James Harris <wopiancitizen123@gmail.com>
2014-11-26 23:30:32 +0000

    Raise crawl to 20

--- website/hb/assets/modules/info/info.php
+++ website/hb/assets/modules/info/info.php
@@ -82,7 +82,7 @@
     # Calculations
     $crawledPer = round($crawled / $users * 100, 1);
     $uncrawledPer = round($uncrawled / $users * 100, 1);
-    $tLeft = seconds2human(($uncrawled/10)*60);
+    $tLeft = seconds2human(($uncrawled/20)*60);
 ?>
 
 <!DOCTYPE html>
