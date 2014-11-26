2a3433d17718a6c18e48c5ad8df408e9f3688394
James Harris <wopiancitizen123@gmail.com>
2014-11-26 19:17:34 +0000

    kuso

--- website/kuso2.php
+++ website/kuso2.php
@@ -8,34 +8,9 @@
 
     echo $uid;
 
-    if ($uid) {
-        $mysqli = new mysqli("localhost","user","password","dbname");
+    $uid = 1;
 
-        if ($mysqli->connect_errno) {
-            printf("Connect failed: %s\n", $mysqli->connect_error);
-            exit();
-        }
+    echo $uid;
 
-        if ($query = $mysqli->query("SELECT username, avatar, rank FROM players WHERE id='".$uid."';")) {
-            if (!$query) {
-                throw new Exception("Database Error [{$mysqli->errno}] {$mysqli->error}");
-                $mysqli->close();
-                exit();
-            }
-
-            while ($res = $query->fetch_assoc()){
-                $_SESSION['loggedUsr'] = $uid;
-                $_SESSION['username'] = $res['username'];
-                $_SESSION['urank'] = $res['rank'];
-                $_SESSION['uavatar'] = "http://10.201.41.64" + $res['avatar'];
-                echo "Session ok: ".$_SESSION['loggedUsr']." ".$_SESSION['username']." ".$_SESSION['urank']." ".$_SESSION['uavatar'].".<br>";
-                echo $uid;
-                setcookie("uid",$uid,time() + (86400*365));
-                echo "Cookie ok: ".$_COOKIE['uid'].".";
-            }
-
-            $mysqli->close();
-        }
-    } else {
-        echo "not ok";
-    }
+    setcookie("uid",$uid,time() + (86400*365));
+    echo "Cookie ok: ".$_COOKIE['uid'].".";
