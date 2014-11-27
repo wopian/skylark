81d1e8a94481f5612a9010d54a6a03162ec6d00a
James Harris <wopiancitizen123@gmail.com>
2014-11-27 00:40:28 +0000

    quick fix

--- website/hb/assets/modules/info/crawl_users.php
+++ website/hb/assets/modules/info/crawl_users.php
@@ -7,7 +7,7 @@
 
     $users = array();
 
-    $sql = "SELECT * FROM `users` WHERE `crawled` != 0 ORDER BY `timestamp` ASC LIMIT 20";
+    $sql = "SELECT * FROM `users` WHERE `crawled` != 0 ORDER BY `crawled` ASC, `timestamp` ASC LIMIT 20";
     if(!$result = $db->query($sql)){
         die('There was an error running the query [' . $db->error . ']');
     }
