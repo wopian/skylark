36816515874dc067d4ae9495050c1afc12276d44
James Harris <wopiancitizen123@gmail.com>
2014-12-01 09:39:15 +0000

    Update info.php

--- website/hb/assets/modules/info/info.php
+++ website/hb/assets/modules/info/info.php
@@ -26,7 +26,8 @@
         $history = array();
         //$history[1] = 0;
         foreach($dbh->query('SELECT * FROM `users`') as $row) {
-            $history[0][] = '['.$row[3]*1000.']';
+            $historyA = $row[3]*1000;
+            $history[0][] = '['.$historyA.']';
             //$history[1]++;
         }
         print_r($history);
