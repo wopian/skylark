00f89c4df49070aa02c858f746d2971bb3813f29
James Harris <wopiancitizen123@gmail.com>
2014-12-02 11:53:37 +0000

    Update index.php

--- website/index.php
+++ website/index.php
@@ -7,7 +7,7 @@
     <link href="/assets/css/main.css" rel="stylesheet">
 </head>
 
-<body id='skrollr-body' data-0='background-color:#e2e2e2;' data-bottom='background-color:#009688;'>
+<body id='skrollr-body' data-0='background-color:#e2e2e2;' data-50p='background-color:#960088;' data-bottom='background-color:#009688;'>
 
     <section>
         <h1>wopian</h1>
