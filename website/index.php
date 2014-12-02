cf2beba6ebbe73e76a97fe85b5d52168cd5e28cb
James Harris <wopiancitizen123@gmail.com>
2014-12-02 11:30:59 +0000

    Update index.php

--- website/index.php
+++ website/index.php
@@ -7,7 +7,7 @@
     <link href="/assets/css/main.css" rel="stylesheet">
 </head>
 
-<body data-0='background-color:rgb(0,0,255);' data-50p='background-color:rgb(255,0,0);'>
+<body data-0='background-color:rgb(255,255,255);' data-50p='background-color:rgb(0,150,136);'>
     Nothing here yet, Hummingbird Tools is located <a href="//hb.wopian.me">here</a>.
 
     <script type="text/javascript" src="/assets/javascript/skrollr.min.js"></script>
