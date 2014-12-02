95f3655f96a786d6275df157835ac943d03c98f4
James Harris <wopiancitizen123@gmail.com>
2014-12-02 11:29:40 +0000

    Update index.php

--- website/index.php
+++ website/index.php
@@ -7,11 +7,9 @@
     <link href="/assets/css/main.css" rel="stylesheet">
 </head>
 
-<body>
+<body data-0='background-color:rgb(0,0,255);' data-50p='background-color:rgb(255,0,0);'>
     Nothing here yet, Hummingbird Tools is located <a href="//hb.wopian.me">here</a>.
-    
-    <div data-0="background-color:rgb(0,0,255);" data-500="background-color:rgb(255,0,0);">WOOOT<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br></div>
-    
+
     <script type="text/javascript" src="/assets/javascript/skrollr.min.js"></script>
     <script type="text/javascript">
         var s = skrollr.init();
