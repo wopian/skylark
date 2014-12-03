165bcdab6f59964dfe19db88ce86979e2b6d509c
James Harris <wopiancitizen123@gmail.com>
2014-12-03 21:43:17 +0000

    test

--- website/index.php
+++ website/index.php
@@ -9,7 +9,10 @@
 
 <body>
 
-    <text id="hello" transform="translate(390, 370)" fill="#ECF0F1" font-weight="900" font-size="120" data-0="    transform: translate(390px, 370px); " data-100="    transform: translate(30px, 400px); " style="-webkit-transform: translate(390px, 370px); transform: translate(390px, 370px);" class="skrollable skrollable-between">hello</text>
+    <section>
+        <h1>wopian</h1>
+        <p>Front-end Web Developer</p>
+    </section>
 
     <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
     <script type="text/javascript" src="/assets/javascript/skrollr.min.js"></script>
