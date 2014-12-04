b7f3f7a0379517b28f51ccc71468b03f88ab92e9
James Harris <wopiancitizen123@gmail.com>
2014-12-04 00:00:00 +0000

    fix

--- website/index.php
+++ website/index.php
@@ -74,8 +74,9 @@
 
                 $string = '';
                 foreach ($languages as $row) {
-                    $string .= "['" . $row[0] . "', " . $row[1] . "]";
+                    $string .= "['" . $row[0] . "', " . $row[1] . "],";
                 }
+                rtrim($string, ",");
 
                 function pieColour() {
                     $colours = [ "607d8b", "e91e63", "03a9f4", "3f51b5", "ff5722", "ffc107", "9c27b0", "00bcd4", "795548", "009688", "e51c23", "9e9e9e", "ff9800", "259b24", "ffeb3b", "cddc39", "8bc34a", "5677fc", "673ab7" ];
