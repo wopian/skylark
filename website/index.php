b4a48132a3365e9468388ef529847cf9d3e8d0ca
James Harris <wopiancitizen123@gmail.com>
2014-12-04 01:02:22 +0000

    fix

--- website/index.php
+++ website/index.php
@@ -11,7 +11,7 @@
 
     <section>
         <h1 data-center="color:rgba(255,255,255,1)"
-            data-40p-top="color:rgba(255,255,255,1)"
+            data-20p-top="color:rgba(255,255,255,1)"
             data-10p-top="color:rgba(255,255,255,0)"
         >wopian</h1>
         <p data-center="color:rgba(255,255,255,1)"
