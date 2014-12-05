663fae9cbf6e0cdc1c8a30b5207c2c0f931238d5
James Harris <wopiancitizen123@gmail.com>
2014-12-05 11:28:16 +0000

    Websocket Secure

--- website/root/assets/javascript/livereload.js
+++ website/root/assets/javascript/livereload.js
@@ -114,7 +114,7 @@ __connector.Connector = Connector = (function() {
     this.WebSocket = WebSocket;
     this.Timer = Timer;
     this.handlers = handlers;
-    this._uri = "ws://" + this.options.host + ":" + this.options.port + "/livereload";
+    this._uri = "wss://" + this.options.host + ":" + this.options.port + "/livereload";
     this._nextDelay = this.options.mindelay;
     this._connectionDesired = false;
     this.protocol = 0;
