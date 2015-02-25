1380d29fde6dec4f7e8b3ead971a921e1e3acce1
James Harris <wopiancitizen123@gmail.com>
2015-02-25 20:18:41 +0000

    options

--- app/assets/javascript/app.js
+++ app/assets/javascript/app.js
@@ -9,6 +9,14 @@
 // grab an element
 var myElement = document.querySelector("header");
 // construct an instance of Headroom, passing the element
-var headroom  = new Headroom(myElement);
+var headroom  = new Headroom(myElement, {
+  "offset": 800,
+  "tolerance": 5,
+  "classes": {
+    "initial": "animated",
+    "pinned": "slideDown",
+    "unpinned": "slideUp"
+  }
+});
 // initialise
 headroom.init();
