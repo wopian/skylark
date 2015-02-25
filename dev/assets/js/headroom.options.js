6bac840e4767ac4b90ab581732e413dd18274e83
James Harris <wopiancitizen123@gmail.com>
2015-02-25 20:15:33 +0000

    .

--- dev/assets/js/headroom.options.js
+++ dev/assets/js/headroom.options.js
@@ -1,16 +1,6 @@
 // grab an element
-var head = document.querySelector("header");
+var myElement = document.querySelector("header");
 // construct an instance of Headroom, passing the element
-var headroom = new Headroom(head, {
-  "offset": 800,
-  "tolerance": 5,
-  "classes": {
-    "initial": "animated",
-    "pinned": "slideDown",
-    "unpinned": "slideUp"
-  }
-});
+var headroom  = new Headroom(myElement);
+// initialise
 headroom.init();
-
-// to destroy
-//headroom.destroy();
