448cac620e0c54027e11a891d9a0f9baf2c94840
James Harris <wopiancitizen123@gmail.com>
2015-03-04 18:59:17 +0000

    .

--- tasks/options/concat.js
+++ tasks/options/concat.js
@@ -1,7 +1,7 @@
 module.exports = {
   dist: {
     files: {
-        'app/assets/javascript/app.js' : ['dev/assets/js/holder.js', 'dev/assets/js/headroom.min.js', 'dev/assets/js/headroom.options.js']
+        'app/assets/javascript/app.js' : ['dev/assets/js/jquery.js', 'dev/assets/js/jquery.lazyload.min.js', 'dev/assets/js/holder.js', 'dev/assets/js/app.js', 'dev/assets/js/headroom.min.js', 'dev/assets/js/headroom.options.js']
     }
   }
 }
