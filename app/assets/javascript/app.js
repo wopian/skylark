fe7405ba6b4174d8704d98b03abe085a0c0b9dfa
James Harris <wopiancitizen123@gmail.com>
2015-03-04 20:03:14 +0000

    .

--- app/assets/javascript/app.js
+++ app/assets/javascript/app.js
@@ -2045,6 +2045,21 @@ Holder.js - client side image placeholders
 });
 */
 
+$(function(){
+
+
+  $('img.lazy').each(function(){
+
+    var img = $(this);
+
+     img.error(function(){
+      img.attr({"data-src": "holder.js/490x710/auto/text:Missing Cover"});
+        Holder.run({});
+      });
+
+  })
+});
+
 $(function() {
     $("img.lazy").lazyload({
         event : "sporty",
