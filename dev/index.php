3990ad0e5eda9823eec5c98a38f2a86cbae8b6fb
James Harris <wopiancitizen123@gmail.com>
2015-03-04 19:55:47 +0000

    .

--- dev/index.php
+++ dev/index.php
@@ -78,7 +78,7 @@
 
             <card>
                 <poster>
-                    <img class="lazy" data-original="holder.js/490x710/auto/text:Tomako Market">
+                    <img class="lazy" data-src="holder.js/490x710/auto/text:Tomako Market">
                     <!--<img src="https://static.hummingbird.me/anime/poster_images/000/007/425/large/ibcBbSO731RyEc.jpg?1416491743">-->
                 </poster>
                 <detail>
