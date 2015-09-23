91cb24c8def7298efd1a5849d323a3b1d2d43bd9
James Harris <wopiancitizen123@gmail.com>
2015-09-24 00:18:45 +0100

    Start 2

---
+++ dev/index.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <!--<title>Wopian &horbar; Hibari</title>-->
+    <title>Hibari</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="/assets/css/base.css" rel="stylesheet">
+
+    <!-- Meta Data -->
+    <meta name="description" content="Hummingbird Tools hosts a variety of tools and stats for Hummingbird.">
+    <meta name="keywords" content="Hummingbird,Tool,Tools,Anime,Manga,API,User Stats,Library,User Library,Cover,Cover Images">
+    <meta name="author" content="James Harris">
+    <meta name="robots" content="NOODP">
+
+    <!--<meta property="og:image" content="//hb.wopian.me/assets/images/avatar_teal.png" />
+    <meta property="og:url" content="hb.wopian.me" />
+    <meta property="og:title" content="Hummingbird Tools" />
+
+    <meta name="twitter:card" content="summary" />
+    <meta name="twitter:site" content="@hb_tools" />
+    <meta name="twitter:title" content="Hummingbird Tools" />
+    <meta name="twitter:description" content="A variety of tools and stats for Hummingbird." />
+    <meta name="twitter:image" content="//hb.wopian.me/assets/images/twitter_teal.png" />
+    <meta name="twitter:url" content="//hb.wopian.me" />-->
+</head>
+
+<body>
+
+    <header>
+        <div>
+            <a>Explore</a>
+            <a>Trends</a>
+            <a>Covers</a>
+        </div>
+
+        <a>Hibari</a>
+
+        <div>
+            <a>Profile</a>
+            <a>Anime</a>
+            <a>Manga</a>
+        </div>
+    </header>
+
+    <section id="content">
+        <main>
+        </main>
+
+        <aside>Side 1</aside>
+        <aside>Side 2</aside>
+    </section>
+
+    <footer>
+        <p>&copy; James Harris, 2015</p>
+        <p>
+            <a>Privacy</a>
+            <a>TOS</a>
+        </p>
+    </footer>
+
+</body>
