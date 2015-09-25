319cad3a6ccad068af2447f0e60c15e199356b95
James Harris <wopiancitizen123@gmail.com>
2015-09-25 18:40:35 +0100

    .

--- src/index.php
+++ src/index.php
@@ -58,9 +58,6 @@
         <a href="http://deploybot.com">
             <img src="https://wopian-hb.deploybot.com/badge/88313865898261/46784.svg" alt="Deployment status from DeployBot">
         </a>
-        <a href="/status" title="Website Uptime Monitoring">
-            <img src="https://www.statuscake.com/App/button/index.php?Track=xn0UgGJI42&Days=1000&Design=6" />
-        </a>
 
         <div>
             <a>Privacy</a>
