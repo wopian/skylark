d01825c81065dcfe9a56dc53945495191f47274a
James Harris <wopiancitizen123@gmail.com>
2015-09-25 17:39:56 +0100

    Add Font V2

--- tasks/options/sass.js
+++ tasks/options/sass.js
@@ -1,8 +1,7 @@
 module.exports = {
     dist: {
         options: {
-            style: 'expanded',
-            check: true,
+            style: 'expanded'
         },
         files: {
             'dist/assets/css/app.css': 'src/assets/scss/base.scss'
