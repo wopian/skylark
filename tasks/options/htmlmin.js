bba5061ace98cda6082cbf906bba62efcff928e9
James Harris <wopiancitizen123@gmail.com>
2015-09-25 16:30:55 +0100

    .

--- tasks/options/htmlmin.js
+++ tasks/options/htmlmin.js
@@ -2,7 +2,11 @@ module.exports = {
     dist: {
         options: {
             removeComments: true,
-            collapseWhitespace: true
+            collapseWhitespace: true,
+            conservativeCollapse: true,
+            removeRedundantAttributes: true,
+            removeEmptyAttributes: true,
+            useShortDoctype: true
         },
         files: {
             'app/index.php': 'dev/index.php'
