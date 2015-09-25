b407b1ce8a90e76dd0a7efe4ddbdd1b8c5008c6d
James Harris <wopiancitizen123@gmail.com>
2015-09-25 12:32:02 +0100

    Update concat.js

--- tasks/options/concat.js
+++ tasks/options/concat.js
@@ -1,7 +1,12 @@
 module.exports = {
+  options: {
+    stripBanners: true,
+  },
   dist: {
-    files: {
+    /*files: {
         'app/assets/javascript/app.js' : ['dev/assets/js/jquery.js', 'dev/assets/js/jquery.lazyload.min.js', 'dev/assets/js/holder.js', 'dev/assets/js/app.js', 'dev/assets/js/headroom.min.js', 'dev/assets/js/headroom.options.js']
-    }
+    }*/
+    src: ['dev/assets/js/jquery.js', 'dev/assets/js/jquery.lazyload.min.js', 'dev/assets/js/holder.js', 'dev/assets/js/app.js', 'dev/assets/js/headroom.min.js', 'dev/assets/js/headroom.options.js'],
+    dest: 'app/assets/javascript/app.js',
   }
 }
