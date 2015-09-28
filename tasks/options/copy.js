8493e387508ce1cf1aa6d66acfa0ad09014ad6b0
James Harris <wopiancitizen123@gmail.com>
2015-09-28 17:39:06 +0100

    Add missing colon

--- tasks/options/copy.js
+++ tasks/options/copy.js
@@ -1,10 +1,10 @@
 module.exports = {
     dist: {
-    files: [{
-        expand: true,
-        cwd: 'src/'
-        src: ['.htaccess', 'robots.txt', 'hello_world.txt'],
-        dest: 'dist/'
-    }]
+        files: [{
+            expand: true,
+            cwd: 'src/',
+            src: ['.htaccess', 'robots.txt', 'hello_world.txt'],
+            dest: 'dist/'
+        }]
     }
 }
