d490954f3c44e58a130cc6ce4d5a95894369bfb9
James Harris <wopiancitizen123@gmail.com>
2015-09-30 14:42:54 +0100

    Update htmlmin.js

--- tasks/options/htmlmin.js
+++ tasks/options/htmlmin.js
@@ -5,7 +5,8 @@ module.exports = {
             collapseWhitespace: true,
         },
         files: {
-            'dist/index.php': 'src/index.php'
+            'dist/index.php': 'src/index.php',
+            'dist/tos.php': 'src/tos.php'
         }
     }
 }
