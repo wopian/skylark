92809a78e350cfa4ef153b496702c2659bf7a953
James Harris <wopiancitizen123@gmail.com>
2015-09-30 14:48:17 +0100

    Update tos.php

--- src/tos.php
+++ src/tos.php
@@ -98,8 +98,8 @@
 
         </main>
 
-        <!--<aside>Side 1</aside>
-        <aside>Side 2</aside>-->
+        <aside>Side 1</aside>
+        <aside>Side 2</aside>
     </section>
 
     <footer>
