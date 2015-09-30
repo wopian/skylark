95072c3e5609ad43d287983ba6178bb773b21704
James Harris <wopiancitizen123@gmail.com>
2015-10-01 00:00:58 +0100

    Update index.php

--- src/index.php
+++ src/index.php
@@ -55,11 +55,10 @@
 
     <footer>
         <div>&copy; James Harris, 2015</div>
-
+        <a id="deploy" href="http://deploybot.com">
+            <img src="https://wopian-hb.deploybot.com/badge/88313865898261/46784.svg" alt="Deployment status from DeployBot">
+        </a>
         <div>
-            <a id="deploy" href="http://deploybot.com">
-                <img src="https://wopian-hb.deploybot.com/badge/88313865898261/46784.svg" alt="Deployment status from DeployBot">
-            </a>
             <a href="/tos#privacy">Privacy</a>
             <a href="/tos">ToS</a>
         </div>
