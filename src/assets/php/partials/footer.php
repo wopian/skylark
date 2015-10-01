2ca6ee90377c698118175fd5365ce73f6232f46a
James Harris <wopiancitizen123@gmail.com>
2015-10-01 10:12:16 +0100

    Create footer.php

---
+++ src/assets/php/partials/footer.php
@@ -0,0 +1,13 @@
+	<footer>
+	
+		<div>&copy; James Harris, 2015</div>
+		<a id="deploy" href="http://deploybot.com">
+			<img src="https://wopian-hb.deploybot.com/badge/88313865898261/46784.svg" alt="Deployment status from DeployBot">
+		</a>
+		<div>
+			<a href="/tos#privacy">Privacy</a>
+			<a href="/tos">ToS</a>
+		</div>
+
+	</footer>
+</body>
