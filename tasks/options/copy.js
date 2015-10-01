4285b29d8b25250b111beff8b93b76c02d391714
James Harris <wopiancitizen123@gmail.com>
2015-10-01 09:30:24 +0100

    Update copy.js

--- tasks/options/copy.js
+++ tasks/options/copy.js
@@ -8,10 +8,10 @@ module.exports = {
             dest: 'dist/'
         },
         {
-            expand: true,
-            cwd: 'src/assets/php/',
-            src: '*.php',
-            dest: 'dist/assets/php'
+            expand: true, 
+            cwd: 'src/assets/php/', 
+            src: ['**'], 
+            dest: 'dist/assets/php/'
         }]
     }
 }
