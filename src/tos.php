793c0a244db3d16b485b59334a472c5cefc4a52b
James Harris <wopiancitizen123@gmail.com>
2015-10-01 10:12:45 +0100

    Update tos.php

--- src/tos.php
+++ src/tos.php
@@ -75,16 +75,4 @@
         <aside>Side 2</aside>
     </section>
 
-    <footer>
-        <div>&copy; James Harris, 2015</div>
-        <a id="deploy" href="http://deploybot.com">
-            <img src="https://wopian-hb.deploybot.com/badge/88313865898261/46784.svg" alt="Deployment status from DeployBot">
-        </a>
-        <div>
-            <a href="/tos#privacy">Privacy</a>
-            <a href="/tos">ToS</a>
-        </div>
-
-    </footer>
-
-</body>
+<? require('./assets/php/partials/footer.php'); ?>
