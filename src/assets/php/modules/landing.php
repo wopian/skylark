c07d240cbc02736f6b09b2fe1026c07b31aa8d13
James Harris <wopiancitizen123@gmail.com>
2015-10-01 12:50:22 +0100

    Update landing.php

--- src/assets/php/modules/landing.php
+++ src/assets/php/modules/landing.php
@@ -1,2 +1,20 @@
 <? ob_start(); ?>
-<p>Dank memes r cool!</p>
+<p>Lorem ipsum dolor sit amet, vel facilisi vituperata eu. Vis ut maiorum comprehensam, omnis fabellas ea duo, eam delenit mandamus salutandi ex. Et qui paulo verterem, eu pro augue nusquam scripserit, ad modo solet nec. Velit impetus eu eos, quot erant laboramus vix cu. Eam sint modus no, est iriure euismod omnesque ex.</p>
+<br><br>
+<p>In sint populo his, vix meis libris utamur in, mei ex meliore voluptua tractatos. Et ius quod viris, option complectitur ea mea, facer tation indoctum per ne. At illum dicit pro, elit primis mea te. Appetere contentiones ad est. Vim et alii soleat mentitum, quem purto pertinax in pro.</p>
+<br><br>
+<p>Duis harum dicant ius ei, mei eu quas labores, mei ei habeo sanctus. Nihil clita ne eum. Veri postea deserunt an est, iriure feugait vix eu. Solet aliquam invenire est at. Nonumy dictas impetus ex vel, cu his diam malis liber.</p>
+<br><br>
+<p>Ad ius persius vulputate, ut ius melius malorum rationibus. Cu verear scriptorem has, quo te nibh velit labores, cum volumus maiestatis quaerendum at. Movet maluisset tincidunt his ea. Ex omnes eruditi omittam vel, ludus dolore vel an.</p>
+<br><br>
+<p>Veri temporibus ea sea, percipit invidunt delicatissimi in vis. Ea purto copiosae quo, eam adhuc omittam cu, option denique fabellas ex quo. No bonorum sententiae sea. No qui wisi libris inciderint, vix stet meliore appetere id. Ex vel falli nostrum, mei te eius perfecto consectetuer.</p>
+<br><br>
+<p>大施た初済ヨ協久回29動ケユ燥万いざ施朝ケモエ掲換ぱ勢徳もあづレ学86質モ会寄よほやー主約ラ他掲イば用選カオテ役並じぽ断集すさち診具多わすべ。齢リヲエ植転モスシ本5座ぽゅじク暮真入リで在更うげ問24代と可頼リナ教造イチホ低朝ト嶋者点複庫レ。正ク非天馨ケ頬転ケ園術躍モシハ直安ミ策台対町天く礎高わルうや判彼精平ラげ。</p>
+<br><br>
+<p>調ッょゅす摘2止チ昨辺融ー蓮2国ったぴ北善ネトコ必2届びを動少ナヱ収整をけ番母わめね書敗髄タカウレ分市以ルメタノ事霊しっ。特びクト特経さ国集む芸週ヌソノツ布氷ツ連外造企ルムイワ活牛質むげラ財8譲平ケ違先ずの都補シエト建概にリ宮体際か挙氷病般なし。開ハシリサ工歴ラい日退ナロノ手率れき知球の授知ヤメワ連給有チ社信ねトぴ右力ミハヱ小70図びじ吹令し。</p>
+<br><br>
+<p>周択つレづ銀署リコ泉海連勝ナ権算れづねぐ強境う処7目ヨコ抗際オ唐太ゃそ書名タレ俊男タセ以名博ーな図紙ハヲ検市け稿正ク満込いじ捜求契しあ。本度ぐス常5陰えち首次げにづも毎権早ネクタヤ勧書関68方ロ発通カミツ開稼ソクエメ子多で開全あかびス不年へち試安ホ権畑跡圧りぽぶげ。</p>
+<br><br>
+<p>掲タ改7禁線かン読記ク形観ふげどゃ作党ラはおね俗世キ視庫の界期るだでょ然刊タルス反上オヒウ再情浜撤こり。急オレテ注厘すリん苦録キソ取朝背りぐの効民ユ州見オソタ更著トイエカ告百とが経撃際月ゅる者迎アレワ加写最巡何ンごリ。商カシ場78力ヲソ調津ニカ田難なあ画8夕じうり着語地るむみぽ氷8羽位さへて摘誉ヤ愛堂期9告覧し然連べほんお要帰歴けぼンイ。</p>
+<br><br>
+<p>企ょれぎ探検キエ混下ゆ敗環毎せり復首よくこ民見いたるぐ魅終ミワリ家然ロチムア特芸図絵ぜルト線惑ルレ警案ム小東づめれせ請走楽めのス。滋ぜぞたぐ華伴密ヘツ国2地ヱヲモレ落感ルユハ属陣メセクト島歳るぱら間天特ーさ置新ヤ可28左ぜひべえ際井スフ春十悲ねのな。中づをドず会院ラ参54由アメセノ第陣えおれ秘図サキオセ写彼2工へぱこ受治なぎおび題均ヌトヒ預子カスリロ川就て千幅採わっ。</p>
