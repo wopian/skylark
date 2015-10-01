ad838059526d32d9212b1b5548f6db864933f2e1
James Harris <wopiancitizen123@gmail.com>
2015-10-01 18:16:28 +0100

    Update landing.php

--- src/assets/php/modules/landing.php
+++ src/assets/php/modules/landing.php
@@ -2,4 +2,5 @@
 <h1>Dank Memes</h1>
 <p>Lorem ipsum dolor sit amet, vel facilisi vituperata eu. Vis ut maiorum comprehensam, omnis fabellas ea duo, eam delenit mandamus salutandi ex. Et qui paulo verterem, eu pro augue nusquam scripserit, ad modo solet nec. Velit impetus eu eos, quot erant laboramus vix cu. Eam sint modus no, est iriure euismod omnesque ex.</p>
 <br><br>
+<h2>じめじめミーム</h2>
 <p>調ッょゅす摘2止チ昨辺融ー蓮2国ったぴ北善ネトコ必2届びを動少ナヱ収整をけ番母わめね書敗髄タカウレ分市以ルメタノ事霊しっ。特びクト特経さ国集む芸週ヌソノツ布氷ツ連外造企ルムイワ活牛質むげラ財8譲平ケ違先ずの都補シエト建概にリ宮体際か挙氷病般なし。開ハシリサ工歴ラい日退ナロノ手率れき知球の授知ヤメワ連給有チ社信ねトぴ右力ミハヱ小70図びじ吹令し。</p>
