48187d65877c98ed596cf9f32bea9819ed3df289
James Harris <wopiancitizen123@gmail.com>
2015-10-06 15:38:30 +0100

    Update footer.php

--- src/assets/php/partials/footer.php
+++ src/assets/php/partials/footer.php
@@ -1,8 +1,14 @@
 <? ob_start(); ?>
 	<footer>
 		<div>&copy; James Harris, 2015</div>
-		<a id="deploy" href="http://deploybot.com">
+		<a class="status" href="http://deploybot.com">
 			<img src="https://wopian-hb.deploybot.com/badge/88313865898261/46784.svg" alt="Deployment status from DeployBot">
+		</a>
+		<a class="status" href="https://magnum.travis-ci.com/wopian/skylark/">
+			<img src="https://magnum.travis-ci.com/wopian/skylark.svg?token=yBoQEdu6zS8uoXWWoq3Q">
+		</a>
+		<a class="status" href="https://codeclimate.com/repos/5612799869568064350010e4/feed">
+			<img src="https://codeclimate.com/repos/5612799869568064350010e4/badges/3596865e43975423fa9a/gpa.svg">
 		</a>
 		<div>
 			<a href="/tos#privacy">Privacy</a>
