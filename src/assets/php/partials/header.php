e160b7347e1b5dcaaa64afda543948c849a8429c
James Harris <wopiancitizen123@gmail.com>
2015-10-08 16:52:19 +0100

    Fix User page

--- src/assets/php/partials/header.php
+++ src/assets/php/partials/header.php
@@ -2,56 +2,51 @@
 <!DOCTYPE html>
 <html>
 <head>
-        <?
-                switch (isset($_GET['page'])) {
-                        case 'user':
-                                $page = 'user';
-                                $username = preg_replace('/[^-a-zA-Z0-9_]/', '', $_GET['user']);
-                                $usernamePlural = (substr($username, -1) == 's') ? '\'' : '\'s';
-                                print('<title>'.$username.' &horbar; Hibari</title>');
-                                break;
-                        default:
-                                print('<title>Hibari</title>');
-                                break;
-                }
-        ?>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link href="/assets/css/a.min.css" rel="stylesheet">
-        <link href="//brick.a.ssl.fastly.net/Fira+Sans:300,400,500" rel="stylesheet">
+  <?
+    if (isset($username)) {
+      print('<title>'.$username.' &horbar; Hibari</title>');
+    } else {
+      print('<title>Hibari</title>');
+    }
+  ?>
 
-        <!-- Meta Data -->
-        <meta name="description" content="Hummingbird Tools hosts a variety of tools and stats for Hummingbird.">
-        <meta name="keywords" content="Hummingbird,Tool,Tools,Anime,Manga,API,User Stats,Library,User Library,Cover,Cover Images">
-        <meta name="author" content="James Harris">
-        <meta name="robots" content="NOODP">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="/assets/css/a.min.css" rel="stylesheet">
+  <link href="//brick.a.ssl.fastly.net/Fira+Sans:300,400,500" rel="stylesheet">
 
-        <!--<meta property="og:image" content="//hb.wopian.me/assets/images/avatar_teal.png" />
-        <meta property="og:url" content="hb.wopian.me" />
-        <meta property="og:title" content="Hummingbird Tools" />
+  <!-- Meta Data -->
+  <meta name="description" content="Hummingbird Tools hosts a variety of tools and stats for Hummingbird.">
+  <meta name="keywords" content="Hummingbird,Tool,Tools,Anime,Manga,API,User Stats,Library,User Library,Cover,Cover Images">
+  <meta name="author" content="James Harris">
+  <meta name="robots" content="NOODP">
 
-        <meta name="twitter:card" content="summary" />
-        <meta name="twitter:site" content="@hb_tools" />
-        <meta name="twitter:title" content="Hummingbird Tools" />
-        <meta name="twitter:description" content="A variety of tools and stats for Hummingbird." />
-        <meta name="twitter:image" content="//hb.wopian.me/assets/images/twitter_teal.png" />
-        <meta name="twitter:url" content="//hb.wopian.me" />-->
+  <!--<meta property="og:image" content="//hb.wopian.me/assets/images/avatar_teal.png" />
+  <meta property="og:url" content="hb.wopian.me" />
+  <meta property="og:title" content="Hummingbird Tools" />
+
+  <meta name="twitter:card" content="summary" />
+  <meta name="twitter:site" content="@hb_tools" />
+  <meta name="twitter:title" content="Hummingbird Tools" />
+  <meta name="twitter:description" content="A variety of tools and stats for Hummingbird." />
+  <meta name="twitter:image" content="//hb.wopian.me/assets/images/twitter_teal.png" />
+  <meta name="twitter:url" content="//hb.wopian.me" />-->
 </head>
 
 <body>
 
-        <header>
-                <div>
-                        <a href="#Explore">Explore</a>
-                        <a href="#Trends">Trends</a>
-                        <a href="#Covers">Covers</a>
-                </div>
+  <header>
+    <div>
+      <a href="#Explore">Explore</a>
+      <a href="#Trends">Trends</a>
+      <a href="#Covers">Covers</a>
+    </div>
 
-                <a href="/">Hibari</a>
+    <a href="/">Hibari</a>
 
-                <div>
-                        <a href="#Profile">Profile</a>
-                        <a href="#Anime">Anime</a>
-                        <a href="#Manga">Manga</a>
-                </div>
-        </header>
+    <div>
+      <a href="#Profile">Profile</a>
+      <a href="#Anime">Anime</a>
+      <a href="#Manga">Manga</a>
+    </div>
+  </header>
