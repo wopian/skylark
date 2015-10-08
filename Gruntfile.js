0e74241ba0db6044919b1d4ad43e51da3240888b
James Harris <wopiancitizen123@gmail.com>
2015-10-08 02:50:39 +0100

    Stuff

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,31 +2,29 @@ var timer = require("grunt-timer");
 
 module.exports = function(grunt) {
 
-    timer.init(grunt, { deferLogs: true, friendlyTime: true });
+  timer.init(grunt, { deferLogs: true, friendlyTime: true });
 
-    function loadConfig(path) {
-        var glob = require('glob');
-        var object = {};
-        var key;
+  function loadConfig(path) {
+    var glob = require('glob');
+    var object = {};
+    var key;
 
-        glob.sync('*', {cwd: path}).forEach(function(option) {
-            key = option.replace(/\.js$/,'');
-            object[key] = require(path + option);
-        });
+    glob.sync('*', {cwd: path}).forEach(function(option) {
+      key = option.replace(/\.js$/,'');
+      object[key] = require(path + option);
+    });
 
-        return object;
-    };
+    return object;
+  };
 
-    var config = { pkg: grunt.file.readJSON('package.json') };
+  var config = { pkg: grunt.file.readJSON('package.json') };
 
-    grunt.loadTasks('tasks');
+  grunt.loadTasks('tasks');
+  grunt.util._.extend(config, loadConfig('./tasks/'));
+  grunt.initConfig(config);
 
-    grunt.util._.extend(config, loadConfig('./tasks/'));
+  require('load-grunt-tasks')(grunt);
 
-    grunt.initConfig(config);
-
-    require('load-grunt-tasks')(grunt);
-
-    grunt.registerTask('default', [/*'concat', 'uglify',*/ 'sass', 'autoprefixer', 'cssmin', 'htmlmin', 'copy', 'imagemin']);
+  grunt.registerTask('default', [/*'concat', 'uglify',*/ 'sass', 'autoprefixer', 'cssmin', 'htmlmin', 'copy', 'imagemin']);
 
 };
