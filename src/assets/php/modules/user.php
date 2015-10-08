c7a3a3906657f3ad2e747815b8654bf622a9dfd9
James Harris <wopiancitizen123@gmail.com>
2015-10-08 16:52:19 +0100

    Fix User page

--- src/assets/php/modules/user.php
+++ src/assets/php/modules/user.php
@@ -1,8 +1,11 @@
-<? 
+<?
+ #Grab user field from URL and preload user info
   $username = preg_replace('/[^-a-zA-Z0-9_]/', '', $_GET['user']);
   $usernamePlural = (substr($username, -1) == 's') ? '\'' : '\'s';
-      
-  require('../partials/header.php?page=user&user=$username'); 
+
+
+
+  require('../partials/header.php'); 
 ?>
 
 <section class="content">
