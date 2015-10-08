11ca6af66ed6efbe033337df0adaeaa95d0bf773
James Harris <wopiancitizen123@gmail.com>
2015-10-08 17:35:24 +0100

    Basic Functionality of User Pages

--- src/assets/php/modules/landing.php
+++ src/assets/php/modules/landing.php
@@ -5,5 +5,7 @@
 <p>This is an automatically built staging version that is up to date with the latest changes.</p>
 <p>All is good if there are 3 green badges at the bottom of the page.</p>
 <br><br>
-<h2>Track your anime viewing habits.</h2>
-<p>調ッょゅす摘2止チ昨辺融ー蓮。</p>
+<h2>Current Pages</h2>
+<ul>
+  <li><a href='/wopian'>User Page</a></li>
+</ul>
--- src/assets/php/modules/user.php
+++ src/assets/php/modules/user.php
@@ -1,17 +1,87 @@
 <?
- #Grab user field from URL and preload user info
-  $username = preg_replace('/[^-a-zA-Z0-9_]/', '', $_GET['user']);
-  $usernamePlural = (substr($username, -1) == 's') ? '\'' : '\'s';
+  #Grab username from URL
+  $user = preg_replace('/[^-a-zA-Z0-9_]/', '', $_GET['user']);
 
+  #Download Hummingbird user profile
+  $apiURL  = 'https://hummingbird.me/api/v1/users/$username';
+  $apiJSON = file_get_contents($apiURL);
+  $apiData = json_decode($apiJSON, true);
 
+  #Redirect to index if invalid username
+  if (empty($apiData['name'])) {
+    header("Location: //$_SERVER['SERVER_NAME']")
+  }
 
-  require('../partials/header.php'); 
+  #Use correct username capitalisation via API
+  $user       = $apiData['name']);
+  $userPlural = (substr($user, -1) == 's') ? '\'' : '\'s';
+
+  #Extract key information
+  $userCover      = $apiData['cover_image'];
+  $userAvatar     = $apiData['avatar'];
+  $userBio        = $apiData['bio'];
+  $userWaifu      = $apiData['waifu'];
+  $userWaifuCheck = $apiData['waifu_or_husbando'];
+  $userTime       = $apiData['life_spent_on_anime']*60;
+  $userLocation   = empty($apiData['location']) ? 'Unknown' : $apiData['location'];
+
+  #Update MySQLi Database
+  $db = new mysqli('localhost', 'bobstudi_hibari', 'eSU1Cq@R;s^I$R{Bgq', 'bobstudi_hibari');
+
+  if ($db->connect_errno > 0) {
+    die('Unable to connect to the database [' . $db->connect_error . ']');
+  }
+
+  $sql = 'INSERT INTO `users` (`username`) VALUES (' . $username . ') ON DUPLICATE KEY UPDATE `name` = ' . $username;
+
+  if(!result = $db->query($sql)) {
+    die('There was an error running the query [' . $db->error . ']');
+  }
+  #End Update MySQLi Database
+
+  function humanSeconds($ss, $recent = false) {
+    $m = (floor(($ss % 3600) / 60) > 0)       ? floor(($ss % 3600) / 60)       . ' minutes' : '';
+    $h = (floor(($ss % 86400) / 3600) > 0)    ? floor(($ss % 86400) / 3600)    . ' hours'   : '';
+    $d = (floor(($ss % 2592000) / 86400) > 0) ? floor(($ss % 2592000) / 86400) . ' days'    : '';
+    $M = (floor($ss / 2592000) > 0)           ? floor($ss / 2592000)           . ' months'  : '';
+    $y = (floor($ss / 31557600) > 0)          ? floor($ss / 31557600)          . ' years'   : '';
+
+    if (strlen($m) > 1 && (strlen($h) > 1 || strlen($d) > 1 || strlen($M) > 1)) {
+      $and = 'and';
+    } else {
+      $and = '';
+    }
+
+    #If no anime watched fill in with 0 minutes
+    if (strlen($m) === '' && strlen($h) === '' && strlen($d) === '' && strlen($M) === '' && strlen($y) === '') {
+      $m = '0 minutes';
+    }
+
+    if ($recent === false) {
+      return '$y $M $d $h $and $m';
+    } else {
+      if ($y != '')                                                     { return "$y"; }
+      elseif ($y == '' && $M != '')                                     { return "$M"; }
+      elseif ($y == '' && $M == '' && $d != '')                         { return "$d"; }
+      elseif ($y == '' && $M == '' && $d == '' && $h != '')             { return "$h"; }
+      elseif ($y == '' && $M == '' && $d == '' && $h == '' && $m != '') { return "$m"; }
+    }
+  }
+
+  require('../partials/header.php');
 ?>
 
 <section class="content">
   <main>
     <?
-      print_r($username . $usernamePlural);
+      print_r($user . $userPlural . ' Details:');
+      echo 'Cover: ' . $userCover;
+      echo 'Avatar: ' . $userAvatar;
+      echo 'Bio: ' . $userBio;
+      echo 'Waifu: ' . $userWaifu;
+      echo 'Location: ' . $userLocation;
+      echo 'Time: ' . $userTime;
+      echo 'Human Time: ' . humanSeconds($userTime);
     ?>
   </main>
 
