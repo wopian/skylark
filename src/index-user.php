97cd9dbeb1fb011bdc6a51bcef370b1e07d4eb6c
James Harris <wopiancitizen123@gmail.com>
2015-10-08 14:34:52 +0100

    Create index-user.php

---
+++ src/index-user.php
@@ -0,0 +1,12 @@
+<? require('./assets/php/partials/header.php'); ?>
+
+<section class="content">
+        <main>
+                <? require('./assets/php/modules/user.php'); ?>
+        </main>
+
+        <aside>Side 1</aside>
+        <aside>Side 2</aside>
+</section>
+
+<? require('./assets/php/partials/footer.php'); ?>
