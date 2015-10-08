d0d42d30dd696632af42248dc5a178aee42042b9
James Harris <wopiancitizen123@gmail.com>
2015-10-08 11:36:40 +0100

    Update landing.php

--- src/assets/php/modules/landing.php
+++ src/assets/php/modules/landing.php
@@ -1,8 +1,10 @@
 <? ob_start(); ?>
 <div class="cta"></div>
 
-<h1>This is an automatically built staging version that is up to date with the latest changes.</h1>
-<p>All is good if there are 3 green badges at the bottom of the page.</p>
+<h1>WeeWooWeeWoo</h1>
+<p>This is an automatically built staging version that is up to date with the latest changes.<p>
+<br><br>
+</p>All is good if there are 3 green badges at the bottom of the page.</p>
 <br><br>
 <h2>Track your anime viewing habits.</h2>
 <p>調ッょゅす摘2止チ昨辺融ー蓮。</p>
