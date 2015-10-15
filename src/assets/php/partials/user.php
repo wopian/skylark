c0a388d1df10372845e0156c04c5ca93bf163552
James Harris <wopiancitizen123@gmail.com>
2015-10-15 10:18:32 +0100

    Update user.php

--- src/assets/php/partials/user.php
+++ src/assets/php/partials/user.php
@@ -50,7 +50,7 @@
   function humanSeconds($minutes) {
     $zero    = new DateTime('@0');
     //Function reports one less day than HB report, so add 1 day (1440 minutes)
-    $offset  = new DateTime('@' . $minutes * 60 + 1440);
+    $offset  = new DateTime('@' . ($minutes + 1440) * 60);
     $diff    = $zero->diff($offset);
     return $diff->format('%Y years, %m months, %d days, %h hours, %i minutes of anime');
   }
