d31b296e78db5374ec0c8988aaf11b56959be51b
James Harris <wopiancitizen123@gmail.com>
2015-10-15 09:47:29 +0100

    Create user.php

---
+++ src/assets/php/partials/user.php
@@ -0,0 +1,23 @@
+<div class="cover" style="background-image: url('<?= $userCover ?>')"></div>
+    <div class="user">
+      <div class="user-avatar" style="background-image: url('<?= $userAvatar ?>')"></div>
+      <p class="user-name"><?= $user ?></p>
+      <div class="user-update">
+        <!--<button href="#">Update Profile</button>-->
+      </div>
+    </div>
+    <p class="user-bio"><?= $userBio ?></p>
+    <div class="user-details">
+      <p>General Info</p>
+    </div>
+    
+    <?
+      print_r($user . $userPlural . ' Details:');
+      //echo '<br><br>Cover: ' . $userCover;
+      //echo '<br><br>Avatar: ' . $userAvatar;
+      //echo '<br><br>Bio: ' . $userBio;
+      echo '<br><br>Waifu: ' . $userWaifu;
+      echo '<br><br>Location: ' . $userLocation;
+      echo '<br><br>Time: ' . $userTime;
+      echo '<br><br>Human Time: ' . humanSeconds($userTime);
+    ?>
