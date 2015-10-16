9ba0517dc3259e55a8c0c986ffe0ca29df63a979
James Harris <wopiancitizen123@gmail.com>
2015-10-16 10:30:04 +0100

    Update app.js

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,21 +1,9 @@
-/*$(function() {
-    $("img.lazy").lazyload({
+$(function() {
+    $(".lazy").lazyload({
         //threshold : 200,
         skip_invisible : false,
         effect : "fadeIn"
     });
-});*/
+});
 
 Holder.run({});
-
-$(function() {
-    $(".lazy").lazyload({
-        event: "sporty",
-        skip_invisible: false,
-        effect: "fadeIn"
-    });
-});
-
-$(window).bind("load", function() {
-    var timeout = setTimeout(function() { $(".lazy").trigger("sporty"); }, 5000);
-});
