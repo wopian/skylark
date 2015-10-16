f4e228e20095f5fc45bf13830fb857b3ff011aad
James Harris <wopiancitizen123@gmail.com>
2015-10-16 10:07:07 +0100

    Update Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -25,7 +25,7 @@ module.exports = function(grunt) {
 
   require('load-grunt-tasks')(grunt);
 
-  grunt.registerTask('default', ['sass', 'autoprefixer', 'cssmin', 'htmlmin', 'copy', 'imagemin', 'uglify', 'concat', 'jshint']);
+  grunt.registerTask('default', ['sass', 'autoprefixer', 'cssmin', 'htmlmin', 'copy', 'imagemin', 'jshint', 'concat', 'uglify']);
 
 };
 
