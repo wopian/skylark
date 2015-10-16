ba6e3df54e222a3289eacf8213e32acdba3ab140
James Harris <wopiancitizen123@gmail.com>
2015-10-16 09:59:18 +0100

    Update app.js

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -4,18 +4,17 @@
         skip_invisible : false,
         effect : "fadeIn"
     });
-});
-*/
+});*/
 
 //Holder.run({});
 
-/*$(function() {
+$(function() {
     $("img.lazy").lazyload({
         event: "sporty",
         skip_invisible: false,
         effect: "fadeIn"
     });
-});*/
+});
 
 /*$(window).bind("load", function() {
     var timeout = setTimeout(function() { $("img.lazy").trigger("sporty") }, 5000);
