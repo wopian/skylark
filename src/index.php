050339f93871f82cd0416341e1af45c09e1a0e31
James Harris <wopiancitizen123@gmail.com>
2015-11-05 12:58:10 +0000

    Update index.php

--- src/index.php
+++ src/index.php
@@ -1,36 +1,18 @@
 <? 
-    function getHeader() {
-        require_once('./assets/php/partials/header.php');
-    }
-    
-    function getFooter() {
-        require_once('./assets/php/partials/footer.php');
-    }
-    
-    function getIndex() {
-        getHeader();
-        require_once('./assets/php/modules/index.php');
-        getFooter();
-    }
-    
-    function getUser() {
-        require_once('./assets/php/partials/user.php');
-        getHeader();
-        require_once('./assets/php/modules/user.php');
-        getFooter();
-    }
-
     if (isset($_GET['page'])) {
         $page = preg_replace('/[^-a-zA-Z0-9_]/', '', $_GET['page']);
         
-        global $page;
-        
         switch ($page) {
             case 'user':
-                getUser();
+                require_once('./assets/php/partials/user.php');
+                require_once('./assets/php/partials/header.php');
+                require_once('./assets/php/modules/user.php');
+                require_once('./assets/php/partials/footer.php');
                 break;
         }
     } else {
-        getIndex();
+        require_once('./assets/php/partials/header.php');
+        require_once('./assets/php/modules/index.php');
+        require_once('./assets/php/partials/footer.php');
     }
 ?>
