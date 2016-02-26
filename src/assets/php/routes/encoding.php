6c86cf86fef445617805ef2a6809931abed67bd8
James Harris <wopiancitizen123@gmail.com>
2016-02-26 22:04:49 +0000

    Add Materialize

---
+++ src/assets/php/routes/encoding.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Encoding Rates">
+
+    <title>Encoding Rates</title>
+
+    <link href="/assets/css/a.min.css" rel="stylesheet" type="text/css" media="screen,projection">
+  </head>
+
+  <body>
+
+    <div class="container">
+      <h1 class="header">Encoding Rates</h1>
+
+      <table class="striped highlight responsive-table">
+        <thead>
+          <tr>
+              <th data-field="id">Name</th>
+              <th data-field="name">Item Name</th>
+              <th data-field="price">Item Price</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          <tr>
+            <td>Alvin</td>
+            <td>Eclair</td>
+            <td>$0.87</td>
+          </tr>
+          <tr>
+            <td>Alan</td>
+            <td>Jellybean</td>
+            <td>$3.76</td>
+          </tr>
+          <tr>
+            <td>Jonathan</td>
+            <td>Lollipop</td>
+            <td>$7.00</td>
+          </tr>
+        </tbody>
+      </table>
+
+    </div>
+  </body>
+</html>
